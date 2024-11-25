1e42a8adef3ae793f4df0380c1e3723f8ca37866
Kyusang <gyugyusong@yonsei.ac.kr>
2024-11-25 14:58:46 +0900

    followObj Fix

--- Assets/Seasons system/Scripts/followObject.cs
+++ Assets/Seasons system/Scripts/followObject.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class followObject : MonoBehaviour
 {
     public GameObject objectToFollow;
+    private void Awake()
+    {
+        objectToFollow = GameObject.Find("Camera Offset");
+    }
 
     void Update()
     {
